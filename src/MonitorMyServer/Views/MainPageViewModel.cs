d6ab17864d374cffed847a800fa3ebb4e18a0ec0
Doods <thibaultherviou@gmail.Com>
2018-09-10 09:07:03 +0200

    toto
    tata

--- src/MonitorMyServer/Views/MainPageViewModel.cs
+++ src/MonitorMyServer/Views/MainPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Doods.Framework.ApiClientBase.Std.Models;
+using Doods.Framework.Mobile.Std.Models;
 using Doods.Framework.Mobile.Std.Mvvm;
 using Doods.Xam.MonitorMyServer.Resx;
 using Doods.Xam.MonitorMyServer.Services;
@@ -9,6 +10,14 @@ namespace Doods.Xam.MonitorMyServer.Views
 {
     public class MainPageViewModel : ViewModel
     {
+        private ViewModelStateItem _viewModelStateItem;
+
+        public ViewModelStateItem ViewModelStateItem
+        {
+            get { return _viewModelStateItem;}
+            private set { SetProperty(ref _viewModelStateItem, value); }
+        }
+
         private readonly ISshService _sshService;
         public MainPageViewModel(ISshService sshService)
         {
@@ -21,9 +30,28 @@ namespace Doods.Xam.MonitorMyServer.Views
         {
             var count = await DataProvider.CountHostAsync();
 
+            if (count <= 0)
+            {
+                ShowErrorHostState();
+              
+            }
 
+            await Login(count);
+
+
+        }
+
+        private void ShowErrorHostState()
+        {
+            var tmp = new ViewModelStateItem(this);
+
+
+        }
+
+        private async Task Login(int count)
+        {
             var con = new SshConnection("192.168.1.73", "pi", "raspberry");
-            _sshService.Init(con,true);
+            _sshService.Init(con, true);
             var toto = new toto() { };
             toto.Handler = toto;
             var val = await _sshService.ExecuteTaskAsync<string>(toto);
