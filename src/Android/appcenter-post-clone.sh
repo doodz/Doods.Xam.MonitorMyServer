f100f97bfa6ae3b7d58d6fc60922c8dfa8fbcdb0
Doods <thibaultherviou@gmail.Com>
2020-01-23 13:16:44 +0100

    Update appcenter-post-clone.sh

--- src/Android/appcenter-post-clone.sh
+++ src/Android/appcenter-post-clone.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
 
-if find $APPCENTER_SOURCE_DIRECTORY -name '*.UITest.csproj';
-then
-	echo "Building UI test projects:"
-	find $APPCENTER_SOURCE_DIRECTORY -name '*.UITest.csproj' -exec msbuild {} \;
-else
-	echo "Can't find UI test project"
-	exit 9999
-fi
+# if find $APPCENTER_SOURCE_DIRECTORY -name '*.UITest.csproj';
+# then
+	# echo "Building UI test projects:"
+	# find $APPCENTER_SOURCE_DIRECTORY -name '*.UITest.csproj' -exec msbuild {} \;
+# else
+	# echo "Can't find UI test project"
+	# exit 9999
+# fi
 # Example: Clone a required repository
 echo "=============================================================================="
 echo "Task         : Clone a required repository"
 echo "Description  : Add other repo"
 echo "Author       : Doods"
 echo "=============================================================================="
- echo "Current APPCENTER_SOURCE_DIRECTORY is $APPCENTER_SOURCE_DIRECTORY"
+echo "Current APPCENTER_SOURCE_DIRECTORY is $APPCENTER_SOURCE_DIRECTORY"
 echo "clone https://github.com/doodz/Doods.Frameworks.Std.git"
 git clone https://github.com/doodz/Doods.Frameworks.Std.git
+echo "print working directory"
+pwd
+ls -la
