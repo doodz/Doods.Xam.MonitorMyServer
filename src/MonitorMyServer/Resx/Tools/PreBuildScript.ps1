94ec73123681a987c2b56524dcb0156317fd3ffc
Doods <thibaultherviou@gmail.Com>
2020-01-24 09:52:45 +0100

    {MMS] : THE : remove ps script

--- src/MonitorMyServer/Resx/Tools/PreBuildScript.ps1
+++ src/MonitorMyServer/Resx/Tools/PreBuildScript.ps1
@@ -1,14 +1,23 @@
 ﻿ Param(
 
      [Parameter(Mandatory=$True)]
-    [string]$ProjectDir,
+    [string]$ProjectDir,   
     [Parameter(Mandatory=$True)]
-    [string]$SolutionDir,
-    [Parameter(Mandatory=$True)]
-    [string]$TargetDir,
-	 [Parameter(Mandatory=$True)]
-    [string]$SolutionPath
+    [string]$TargetDir
 )
+
+ # Param(
+
+     # [Parameter(Mandatory=$True)]
+    # [string]$ProjectDir,
+    # [Parameter(Mandatory=$True)]
+    # [string]$SolutionDir,
+    # [Parameter(Mandatory=$True)]
+    # [string]$TargetDir,
+	 # [Parameter(Mandatory=$True)]
+    # [string]$SolutionPath
+# )
+
 [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
 <#$nupkgs = Get-ChildItem -Recurse -Filter *.nupkg -Path "$SolutionDir\packages" 
 $nuspecs = $nupkgs | %{ [IO.Compression.ZipFile]::OpenRead($_.FullName).Entries | where {$_.Fullname.EndsWith('.nuspec')} } 
@@ -23,7 +32,7 @@ $metadata = $nuspecs | %{
 $metadata | %{ '{0} {1}{4}Autor(en): {2}{4}Lizenz: {3}{4}{4}' -f $_.Title, $_.Version, $_.Authors, $_.LicenseUrl, [Environment]::NewLine } | Out-File "$ProjectDir\Resx\ThirdPartyLicenseOverview.txt"
 #>
 #Get-Content $SolutionPath | where { $_ -match "Project.+, ""(.+csproj)""," }| Out-File "$ProjectDir\Resx\test.txt"
-Get-Content $SolutionPath | where { $_ -match "Project.+, ""(.+csproj)""," } | foreach { $matches[1] } | % {Get-Content $SolutionDir$_ |  where { $_ -match "<PackageReference Include=.+Version="} } | Sort-Object -Unique | Out-File "$ProjectDir\Resx\ThirdPartyLicenseOverview_doods.txt"
+Get-Content $SolutionDir | where { $_ -match "Project.+, ""(.+csproj)""," } | foreach { $matches[1] } | % {Get-Content $SolutionDir$_ |  where { $_ -match "<PackageReference Include=.+Version="} } | Sort-Object -Unique | Out-File "$ProjectDir\Resx\ThirdPartyLicenseOverview_doods.txt"
 
 
 #Get-Content $SolutionPath | where { $_ -match "Project.+, ""(.+)""," } | foreach { $matches[1] } | % {Get-Content $SolutionDir$_ | Find "<PackageReference Include" } | Sort-Object -Unique | Out-File "$ProjectDir\Resx\ThirdPartyLicenseOverview_doods.txt"
@@ -31,3 +40,14 @@ Get-Content $SolutionPath | where { $_ -match "Project.+, ""(.+csproj)""," } | f
 
 # powershell.exe -ExecutionPolicy Bypass -File $(ProjectDir)\Resx\Tools\PreBuildScript.ps1  $(ProjectDir) $(SolutionDir) $(TargetDir) $(SolutionPath)
 # "powershell.exe -ExecutionPolicy Bypass -File C:\GitHub\Doods.Xam.MonitorMyServer\src\MonitorMyServer\\Resx\Tools\PreBuildScript.ps1  C:\GitHub\Doods.Xam.MonitorMyServer\src\MonitorMyServer\ C:\GitHub\Doods.Xam.MonitorMyServer\ C:\GitHub\Doods.Xam.MonitorMyServer\src\MonitorMyServer\bin\Debug\netstandard2.0\ C:\GitHub\Doods.Xam.MonitorMyServer\Doods.Xam.MonitorMyServer.sln
+
+# save
+#powershell.exe -ExecutionPolicy Bypass -File $(ProjectDir)\Resx\Tools\PreBuildScript.ps1  $(ProjectDir) $(SolutionDir) $(TargetDir) $(SolutionPath)
+#$(ProjectDir) C:\GitHub\Doods.Xam.MonitorMyServer\src\MonitorMyServer
+#*$(SolutionDir) C:\GitHub\Doods.Xam.MonitorMyServer
+#$(TargetDir) C:\GitHub\Doods.Xam.MonitorMyServer\src\MonitorMyServer\bin\Release\netstandard2.0
+#*$(SolutionPath) C:\GitHub\Doods.Xam.MonitorMyServer\Doods.Xam.MonitorMyServer.sln
+
+#Get-Content $SolutionPath | where { $_ -match "Project.+, ""(.+csproj)""," } | foreach { $matches[1] } | % {Get-Content $SolutionDir$_ |  where { $_ -match "<PackageReference Include=.+Version="} } | Sort-Object -Unique | Out-File "$ProjectDir\Resx\ThirdPartyLicenseOverview_doods.txt"
+
+
