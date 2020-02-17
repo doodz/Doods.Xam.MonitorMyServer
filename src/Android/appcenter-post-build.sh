68e220fbf094ea988d6d557bce3039a2d14f5d9c
Doods <thibaultherviou@gmail.Com>
2020-02-17 12:34:22 +0100

    Update appcenter-post-build.sh

--- src/Android/appcenter-post-build.sh
+++ src/Android/appcenter-post-build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-#
-# Report build status next to github commit.
-#
-# Adjust settings in github.sh file
-#
-# Contributed by: Mirosław Zawisza
-# https://zeyomir.github.io
+echo "=============================================================================="
+echo "Task         : Report build status next to github commit."
+echo "Description  : Add other repo"
+echo "Author       : Doods"
+echo "=============================================================================="
 
 source github.sh
+echo "debug ==>"
+echo "APPCENTER_BUILD_ID $APPCENTER_BUILD_ID"
+echo "access token =$GITHUB_TOKEN"
 
+echo "<== debug"
 if [ "$AGENT_JOBSTATUS" != "Succeeded" ]; then
     github_set_status_fail
 else
