412ef6e14838f1f7d71f30fdcd2fe9986ede8911
Doods <thibaultherviou@gmail.Com>
2020-02-18 09:59:38 +0100

    Update appcenter-post-build.sh

--- src/Android/appcenter-post-build.sh
+++ src/Android/appcenter-post-build.sh
@@ -11,14 +11,20 @@ find_first_Apk()
     ls $APPCENTER_OUTPUT_DIRECTORY/.apk| sort -n | head -1
 
 }
-source github.sh
 
-if [ "$AGENT_JOBSTATUS" != "Succeeded" ]; then
-    github_set_status_fail
-else
+set_github_processes()
+{
     github_set_status_success
     github_set_release
     local url = github_find_asset_url
     local apkFile = find_first_Apk
     github_upload_release_asset uploadUrl=url filePath=apkFile
+}
+
+source github.sh
+
+if [ "$AGENT_JOBSTATUS" != "Succeeded" ]; then
+    github_set_status_fail
+else
+    set_github_processes
 fi
