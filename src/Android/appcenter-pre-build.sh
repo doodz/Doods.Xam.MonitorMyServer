ed3a27982e6edd4132d87faa6a35ca78b661d95a
Doods <thibaultherviou@gmail.Com>
2020-02-20 09:36:31 +0100

    set versionName with branche_buildnumber

--- src/Android/appcenter-pre-build.sh
+++ src/Android/appcenter-pre-build.sh
@@ -6,13 +6,14 @@ echo "Author       : Doods"
 echo "=============================================================================="
 
 
+
 ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/src/Android/Properties/AndroidManifest.xml
 INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/src/iOS/Info.plist
 
 if [ -e "$ANDROID_MANIFEST_FILE" ]
 then
     echo "Updating version name to ${APPCENTER_BRANCH}_${APPCENTER_BUILD_ID} in AndroidManifest.xml"
-    sed -i '' 's/versionName="[0-9.]*"/versionName="'${APPCENTER_BRANCH}_${APPCENTER_BUILD_ID}'"/' $ANDROID_MANIFEST_FILE
+    sed -i '' 's/versionName=".*"/versionName="'${APPCENTER_BRANCH}_${APPCENTER_BUILD_ID}'"/' $ANDROID_MANIFEST_FILE
 
     echo "File content:"
     cat $ANDROID_MANIFEST_FILE
