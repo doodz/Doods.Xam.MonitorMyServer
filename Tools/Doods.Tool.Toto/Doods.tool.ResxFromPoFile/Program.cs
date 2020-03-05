62e503303a6bcd22b94f3fa891d8e719344fd6eb
Doods <thibaultherviou@gmail.Com>
2020-03-05 14:45:39 +0100

    update resx and tools lst
    update resx and tools lst

---
+++ Tools/Doods.Tool.Toto/Doods.tool.ResxFromPoFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Doods.tool.ResxFromPoFile
+{
+    class Program
+    {
+      
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello doods!");
+            new Class1().Process();
+            new UpdatePoFiles().Process();
+
+        }
+
+       
+    }
+}
