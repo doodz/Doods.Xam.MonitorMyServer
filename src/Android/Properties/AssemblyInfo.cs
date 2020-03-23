f561f9df6c73bf7b80e378872b10b57ed73e48e7
Doods <thibaultherviou@gmail.Com>
2020-03-23 14:03:07 +0100

    [MMS] : doods : update tools, go to androidx

--- src/Android/Properties/AssemblyInfo.cs
+++ src/Android/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Doods.Xam.MonitorMyServer.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Doodz Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
