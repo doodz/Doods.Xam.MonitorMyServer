a3ed29b122ca12640a5f0f8f447a11c732cfd0a3
Doods <thibaultherviou@gmail.Com>
2020-05-25 14:26:09 +0200

    ... ...

--- src/Android/appcenter-post-clone.sh
+++ src/Android/appcenter-post-clone.sh
@@ -13,23 +13,23 @@ echo "Author       : Doods"
 echo "=============================================================================="
 MY_CONFIG=$APPCENTER_SOURCE_DIRECTORY/src/Android/Assets/App.config
 echo MobileCenterKey__
-sed -i '' 's/MobileCenterKey__/'$MobileCenter_Key'/' $MY_CONFIG
+sed -i '' 's/"MobileCenterKey__"/"'$MobileCenter_Key'"/' $MY_CONFIG
 #sed -i 's/MobileCenterKey__/'$MobileCenter_Key'/' $MY_CONFIG
 echo RewardedVideoKey__
-sed -i '' 's/RewardedVideoKey__/'$RewardedVideo_Key'/' $MY_CONFIG
+sed -i '' 's/"RewardedVideoKey__"/"'$RewardedVideo_Key'"/' $MY_CONFIG
 #sed -i 's/RewardedVideoKey__/'$RewardedVideo_Key'/' $MY_CONFIG
 echo AdsKey__
-sed -i '' 's/AdsKey__/'$Ads_Key'/' $MY_CONFIG
+sed -i '' 's/"AdsKey__"/"'$Ads_Key'"/' $MY_CONFIG
 #sed -i 's/AdsKey__/'$Ads_Key'/' $MY_CONFIG
 echo AppAdsKey__
-sed -i '' 's/AppAdsKey__/'$AppAds_Key'/' $MY_CONFIG
+sed -i '' 's/"AppAdsKey__"/"'$AppAds_Key'"/' $MY_CONFIG
 #sed -i 's/AppAdsKey__/'$AppAds_Key'/' $MY_CONFIG
 
 if [ -z "$ModeOmvOnly_Key" ]
     then
         echo "ModeOmvOnlyKey__ evoided in App.config default False"      
     else
-		sed -i '' 's/ModeOmvOnlyKey__/'$ModeOmvOnly_Key'/' $MY_CONFIG
+		sed -i '' 's/"ModeOmvOnlyKey__"/"'$ModeOmvOnly_Key'"/' $MY_CONFIG
 		#sed -i 's/ModeOmvOnlyKey__/'$ModeOmvOnly_Key'/' $MY_CONFIG
 fi
 cat $MY_CONFIG
