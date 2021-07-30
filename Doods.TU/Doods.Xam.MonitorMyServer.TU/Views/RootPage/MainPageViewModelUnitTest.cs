2522364a70d3c4f3c0a899af04237429ce067b6e
Doods <thibaultherviou@gmail.Com>
2021-07-30 18:40:31 +0200

    [TU] : doods : add tu

--- Doods.TU/Doods.Xam.MonitorMyServer.TU/Views/RootPage/MainPageViewModelUnitTest.cs
+++ Doods.TU/Doods.Xam.MonitorMyServer.TU/Views/RootPage/MainPageViewModelUnitTest.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
 using Doods.Xam.MonitorMyServer.Services;
 using Doods.Xam.MonitorMyServer.Views;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -22,7 +24,13 @@ namespace Doods.Xam.MonitorMyServer.TU.Views.RootPage
             Assert.IsNull(obj.MemoryUsage);
             Assert.IsNull(obj.Processes);
             Assert.IsNull(obj.Upgradables);
-           
+
+            Assert.IsNotNull(obj.ChangeHostCmd);
+            Assert.IsNotNull(obj.ShowTestsPageCmd);
+
+            Assert.IsNotNull(obj.ManageHostsCmd);
+            Assert.IsNotNull(obj.ShowProcessesCmd);
+            Assert.IsNotNull(obj.UpdatesCmd);
         }
 
 
@@ -49,6 +57,48 @@ namespace Doods.Xam.MonitorMyServer.TU.Views.RootPage
             configurationMock.Setup(m => m.AdsKey).Returns("MyAdd");
             var obj = new MainPageViewModel(sshService.Object, configurationMock.Object);
             Assert.IsNotNull(obj.Title);
+        }
+
+        [TestMethod]
+        public void ChangeHostCmd()
+        {
+            bool called = false;
+
+            var mockA = new Mock<IConnctionService>();
+            mockA.Setup(x => x.ChangeHostTask()).Returns(() =>
+                {
+                    called = true;
+                    return Task.FromResult(true);
+                }
+            );
+
+
+            LocalAutoMock = AutoMock.GetLoose(cfg => cfg.RegisterMock(mockA));
+            SetMockContainer();
+
+            var sshService = new Mock<ISshService>();
+            var configurationMock = new Mock<Doods.Framework.Std.IConfiguration>();
+            configurationMock.Setup(m => m.AdsKey).Returns("MyAdd");
+            var obj = new MainPageViewModel(sshService.Object, configurationMock.Object);
+            Assert.IsNotNull(obj);
+            Assert.IsNull(obj.Groups);
+            Assert.IsNull(obj.Hostnamectl);
+            Assert.IsNull(obj.MemoryUsage);
+            Assert.IsNull(obj.Processes);
+            Assert.IsNull(obj.Upgradables);
+
+            Assert.IsNotNull(obj.ChangeHostCmd);
+            Assert.IsNotNull(obj.ShowTestsPageCmd);
+
+            Assert.IsNotNull(obj.ManageHostsCmd);
+            Assert.IsNotNull(obj.ShowProcessesCmd);
+            Assert.IsNotNull(obj.UpdatesCmd);
+            Assert.IsTrue(obj.ChangeHostCmd.CanExecute(null));
+
+            obj.ChangeHostCmd.Execute(null);
+            mockA.Verify(c => c.ChangeHostTask(), Times.Once);
+            Assert.IsTrue(called);
+
         }
     }
 }
