973754d1ea7baedbbdbea387f88f0033d2865394
Doods <thibaultherviou@gmail.Com>
2021-11-25 16:26:13 +0100

    [MMS/TU] : doods : add TU

---
+++ Doods.TU/Doods.Xam.MonitorMyServer.TU/Views/Nas/PackageUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using Doods.Xam.MonitorMyServer.Data.Nas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Doods.Xam.MonitorMyServer.TU.Views.Nas
+{
+    [TestClass]
+    public class PackageUnitTest
+    {
+        [TestMethod]
+        public void Create()
+        {
+            var obj = new Package();
+            Assert.IsNotNull(obj);
+            Assert.IsNull(obj.Name);
+            Assert.IsNull(obj.Desc);
+            Assert.IsNull(obj.Status);
+            Assert.IsNull(obj.Source);
+            Assert.IsTrue(obj.IsSelected);
+        }
+
+        [TestMethod]
+        public void SetDeviceName()
+        {
+            var deviceName = "you";
+            var obj = new Package();
+            obj.Name = deviceName;
+            Assert.IsNotNull(obj);
+            Assert.AreEqual(deviceName,obj.Name);
+            Assert.IsNull(obj.Desc);
+            Assert.IsNull(obj.Status);
+            Assert.IsNull(obj.Source);
+            Assert.IsTrue(obj.IsSelected);
+
+        }
+    }
+}
---
+++ Doods.TU/Doods.Xam.MonitorMyServer.TU/Views/Nas/SharedFolderUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Doods.Xam.MonitorMyServer.Data.Nas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Doods.Xam.MonitorMyServer.TU.Views.Nas
+{
+    [TestClass]
+    public class SharedFolderUnitTest
+    {
+        [TestMethod]
+        public void Create()
+        {
+            var obj = new SharedFolder();
+            Assert.IsNotNull(obj);
+            Assert.IsNull(obj.Name);
+            Assert.IsNull(obj.Description);
+            Assert.IsNull(obj.Type);
+            Assert.IsNull(obj.Volume);
+            Assert.IsNotNull(obj.Uuid);
+          
+        }
+    }
+}
